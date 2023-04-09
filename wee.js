b3303cf1c6811e9eba267317af4932eb1118fbf6
alex 3 <hi@thatalex.dev>
2023-04-09 17:05:30 -0400

    totally forgot to remove my quick test file lol

--- wee.js
+++
@@ -1,8 +0,0 @@
-const categories = require('./src/category');
-
-(async () => {
-    console.log(await categories({
-        apikey: "pub_yU31XDgLD7Q4QKT7sRuUNfxmTv0cRlcx",
-        categoryId: "minecraft"
-    }))
-})()
